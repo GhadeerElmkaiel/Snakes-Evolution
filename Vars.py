4789a56185b46817e58b697c3dda72a5327f52e8
GhadeerElmkaiel <ghadeer.elmkaiel@gmail.com>
2020-11-13 14:40:24 +0300

    Explaining parameters in Vars.py

--- Vars.py
+++ Vars.py
@@ -12,15 +12,18 @@ blue = (0,0,255)
 
 FPS = 20
 
+# Width and hight of the full window (with neural network and info)
 fullWinWidth = 1400
 fullWinHieght = 800
 fullWinDimentions =(fullWinWidth, fullWinHieght)
 
+# Width and hight of the game simulation window (without the neural network and info)
 playingWinWidth = 750
 playingWinHieght = 750
 #playingWinDimentions = (playingWinWidth, playingWinHieght)
 playingWinPos = 25
 
+# Size of each cell
 blockSize = 10
 mainSpeed = blockSize
 wallThikness = 6
@@ -30,6 +33,8 @@ maxApplesNum = 200
 applesToshow = [0 for i in range(maxApplesNum)]
 maxScore = 0
 numOfDeadSnakes = 0
+
+# The number of cells to add after each point (apple)
 growthRate = 4
 initialPos = [int(playingWinWidth/20)*10,int(playingWinHieght/20)*10]
 
@@ -45,9 +50,11 @@ allowedRepetition = 5
 numOfMovesToCheckRepetition = 20
 maxMovesWithoutApples = (playingWinWidth + playingWinHieght)/blockSize *1.2
 
-
-genNum = 1
+# The size of each generation (num of snakes to test)
 genSize = 1000
+
+# Initializing the environment
+genNum = 1
 numOfMoves = 0
 bestScore = 0
 bestScoreThisGen = 0
